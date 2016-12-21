d27f27bbbc42a5263e84c7ac1f80e7b3e56349be
michalgnat <michal.gnat@gmail.com>
2016-12-21 13:21:42 -0500

    Update Email_Retriever_Text_Creator.py

--- draft/Email_Retriever_Text_Creator.py
+++ draft/Email_Retriever_Text_Creator.py
@@ -5,7 +5,7 @@ from os import listdir
 from os.path import isfile, join
 
 def file_creator(payload): #TO DO: Make a script to change file names to update
-    onlyfiles = [f for f in listdir('/home/mike/Python/Automated_Trader/Recieved_Email/') if isfile(join('/home/mike/Python/Automated_Trader/Recieved_Email/', f))]
+    onlyfiles = [f for f in listdir('PWD') if isfile(join('PWD', f))]
     onlyfiles.sort()
     version = 1;
     filename = 'Email_Trade_Alert1.txt'
@@ -19,19 +19,15 @@ def file_creator(payload): #TO DO: Make a script to change file names to update
                 filename[-5]='%d'%version
                 filename = "".join(filename)
                 
-        with open(os.path.join('/home/mike/Python/Automated_Trader/Recieved_Email/', filename), 'w') as file1:
+        with open(os.path.join('PWD', filename), 'w') as file1:
             file1.write(payload)
             file1.close()
         break
 
-#def connect_gmail(login, password):
-    
-    
-
 def main():
     conn = imaplib.IMAP4_SSL('imap.gmail.com')
     subject = 'New Trade Alert'
-    conn.login('michal.gnat@gmail.com', 'aqjejrhozjeimzzc')
+    conn.login('LOGINNAME@gmail.com', 'PASSWORD')
 
     conn.select('Inbox')
     typ, data = conn.search(None,'(SUBJECT "%s")' % subject) #gathers all the emails and presents them in numbers
@@ -48,14 +44,3 @@ def main():
     file_creator(msg.get_payload())
 
 main()
-##f = open('Trade_Alert_email.txt', 'w')
-##f.write(msg.get_payload())
-##f.close()
-
-#for num in data[0].split():
-#   print num
-##    typ, data = conn.fetch(num,'(RFC822)')
-##    print data
-##    msg = email.message_from_string(data[0][1])
-##    print msg
-##    typ, data = conn.store(num,'-FLAGS','\\Seen')
